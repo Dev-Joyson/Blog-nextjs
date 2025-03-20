59aeecbfbfabbd1213df85fd38b3bcdf66e4435b
Joyson <cj4dev@gmail.com>
2025-03-20 21:50:37 +0530

    modified posts fetching from api

---
+++ models/postModel.js
@@ -0,0 +1,16 @@
+import { Schema , model, models} from "mongoose";
+import { title } from "process";
+
+const postSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+}, {toJSON: {virtuals: true}});
+
+postSchema.virtual('short_description').get(function() {
+    return this.description.substr(0,50)+'...'
+})
+
+const PostModel = models.POST || model('Post', postSchema);
+
+export default PostModel;
